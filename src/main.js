a7bbedf44dea03325cf67da53a79adba5c4b9daa
Everton <everton.h.g.fonseca@gmail.com>
2022-09-08 10:04:55 -0300

    dependencia axios instalada (falta configurar)

--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
 import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import App from '@/App.vue'
+import router from '@/router'
+import store from '@/store'
+import '@/plugins/axios';
+import '@/plugins/vee-validate';
 import '@/assets/css/tailwind.css'
-import '@/plugins/vee-validate.js';
 
 
 Vue.config.productionTip = false

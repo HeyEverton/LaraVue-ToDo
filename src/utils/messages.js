5f8cd3b3e3e8740fe7d5f5935798165f5e72240f
Everton <everton.h.g.fonseca@gmail.com>
2022-09-12 20:29:22 -0300

    exception adicionada

--- src/utils/messages.js
+++ src/utils/messages.js
@@ -2,7 +2,9 @@ export default {
     ServerError: 'Ops! Alguma coisa deu errado.',
     LoginInvalidException: 'Ops! E-mail e/ou senha inválidos.',
     UserHasBeenTakenException: 'Ops! Parece que este e-mail já existe.',
-    VerifyEmailTokenInvalidException: 'Ops! parece que este token está inválido.',
-    UserNotFoundException: 'Ops! parece que este usuário não existe.',
-    ResetPasswordTokenInvalidException: 'Ops! parece que o token é inválido.',
+    VerifyEmailTokenInvalidException: 'Ops! Parece que este token está inválido.',
+    UserNotFoundException: 'Ops! Parece que este usuário não existe.',
+    ResetPasswordTokenInvalidException: 'Ops! Parece que o token é inválido.',
+    AuthorizationException: 'Ops! Parece que essa lista é de outro usuário.',
+    
 };
